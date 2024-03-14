7a19eedf006a71e58ff483e5f00e1bdf708dced8
Gayanath Lakmevan Silva <gayanathlakmevansilva@gmail.com>
2024-03-14 12:18:00 +0530

    add files

--- src/main/java/org/springmvc/Main.java
+++ src/main/java/org/springmvc/Main.java
@@ -1,7 +1,27 @@
 package org.springmvc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springmvc.controller.EmployeeController;
+import org.springmvc.dao.EmployeeDao;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-servlet.xml");
+        EmployeeDao dao = ctx.getBean("dao", EmployeeDao.class);
+        // dao.save(new org.springmvc.entity.Employee(102, "Amit", 35000, "Java Developer"));
+        // dao.update(new org.springmvc.entity.Employee(102, "Amit2", 50000, "Java Developer"));
+        // dao.delete(102);
+//        System.out.println("Employee Details");
+//        for (org.springmvc.entity.Employee e : dao.getEmployees()) {
+//            System.out.println(e.getName());
+//        }
+//        System.out.println(dao.getEmpById(101).getDesignation());
+
+        EmployeeController controller = ctx.getBean("controller", EmployeeController.class);
+//        controller.save(new org.springmvc.entity.Employee(103, "Amit3", 350050, "Java Developer"));
+        //  controller.editsave(new org.springmvc.entity.Employee(103, "Amit3", 50000, "Java Developer"));
+
     }
 }
--- src/main/java/org/springmvc/dao/EmployeeDao.java
+++ src/main/java/org/springmvc/dao/EmployeeDao.java
@@ -1,12 +1,53 @@
 package org.springmvc.dao;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class EmpDao {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springmvc.entity.Employee;
+
+public class EmployeeDao {
 
     JdbcTemplate template;
 
     public void setTemplate(JdbcTemplate template) {
         this.template = template;
+    }
+
+    public int save(Employee emp) {
+        String sql="insert into Emp(id,name,salary,designation) values('"+emp.getId()+"','"+emp.getName()+"',"+emp.getSalary()+",'"+emp.getDesignation()+"')";
+        return template.update(sql);
+    }
+
+    public List<Employee> getEmployees() {
+        return template.query("select * from Emp",new RowMapper<Employee>(){
+            public Employee mapRow(ResultSet rs, int row) throws SQLException {
+                Employee e=new Employee();
+                e.setId(rs.getInt(1));
+                e.setName(rs.getString(2));
+                e.setSalary(rs.getFloat(3));
+                e.setDesignation(rs.getString(4));
+                return e;
+            }
+        });
+    }
+
+    public Employee getEmpById(int id) {
+        String sql="select * from Emp where id=?";
+        return template.queryForObject(sql, new Object[]{id},new BeanPropertyRowMapper<Employee>(Employee.class));
+    }
+
+    public int update(Employee emp) {
+        String sql="update Emp set name='"+emp.getName()+"', salary="+emp.getSalary()+",designation='"+emp.getDesignation()+"' where id="+emp.getId()+"";
+        return template.update(sql);
+    }
+
+    public int delete(int id) {
+        String sql="delete from Emp where id="+id+"";
+        return template.update(sql);
     }
 }
